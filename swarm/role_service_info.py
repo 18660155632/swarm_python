eb392c6688b96f0e7b268cd1f7b1caa0a461881a
root <18660155632@163.com>
2017-10-11 17:24:58 +0800

    列出用户的服务、任务信息页面所需方法基本实现

--- swarm/role_service_info.py
+++ swarm/role_service_info.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from swarm_manage import ConnDocker
 import json
 
@@ -5,6 +6,13 @@ host = '172.16.1.111'
 swarm_conn = ConnDocker(host)
 
 def get_service_info(role = None,):
+    '''
+    某用户登录后，根据其所属角色，列出此角色创建的所有服务、任务的详细信息
+    传入参数为角色名role，如果是admin用户，role = None
+    如果是非admin用户，输出是一个字典。
+    如果是admin用户，输出的是一个列表。列表的元素是各个非admin用户的字典。
+    字典样本参考json_tmp/role_service_task
+    '''
     if role != None:
         my_services_names = swarm_conn.list_service_name_role(role).values()[0]
         service_task_info = []
@@ -24,10 +32,28 @@ def get_service_info(role = None,):
         return [get_service_info(y) for y in swarm_conn.list_service_name_role().keys()]
 
 def remove_service(service_name):
+    '''
+    不解释:)
+    '''
     swarm_conn.remove_service(service_name)
 
 def scale_service(service_name,new_replicas = 1):
+    '''
+    不解释:)
+    '''
     swarm_conn.scal_service(service_name,new_replicas)
+
+def list_service_all_attrs(service_name):
+    '''
+    不解释:)
+    '''
+    swarm_conn.list_service_attrs(service_name)
+
+def list_service_task_attrs(service_name):
+    '''
+    不解释:)
+    '''
+    swarm_conn.list_tasks(service_name)
 
 
 if __name__ == '__main__':
